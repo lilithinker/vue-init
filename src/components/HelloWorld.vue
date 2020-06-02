de27d6b243f4c9b53d2fdb2fc2b0e5fd75931177
lilithinker <lilithinker@gmail.com>
2020-06-02 22:48:54 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <input type="text" v-model="msg">
+  <div>{{reverseMsg}}</div>
+  <div>{{str}}</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      str: ''
+    }
+  },
+  methods:{
+    doSomething(){
+      console.log('do something');
+    }
+  },
+  computed:{
+    reverseMsg:function(){
+      return this.msg.split('').reverse().join('');
+    }
+  },
+  watch:{
+    msg:function(val){
+      this.str = val + ' nihao123';
+    }
+  },
+  filters:{
+    up:function(word){
+      return word.toUpperCase();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
